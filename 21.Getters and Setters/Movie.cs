14bcc9dd73bf63142546deb4729d2342c55df5ba
A253875 <A253875@LSBSA-65LG3J3>
2024-01-10 16:14:58 +0200

    Add project files.

---
+++ 21.Getters and Setters/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Getters_and_Setters
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;// now can only be set by getters and setters, for sucuring our app from nonsensical entries
+                              //constructor
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;// now only using the getters and setters values
+        }
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                    rating = value;
+                else
+                {
+                    rating = "NR";
+
+                }
+            }
+        }
+    }
+}
---
+++ 21.Getters and Setters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Getters_and_Setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie moviel = new Movie("Titanic", "Jim", "PG-13");//new movei object using the construtor
+            Movie movie2 = new Movie("Unreleased", "Evans", "PG");
+            Console.WriteLine(moviel.Rating);
+            Console.ReadLine();
+        }
+    }
+}
